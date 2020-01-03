183add96c4c0d398c610364152c269013f85b13f
Victor Juan Jimenez Flores <vj.jimenez96@gmail.com>
2020-01-03 16:08:47 -0500

    commitv3

--- server/chatbot/config.js
+++ server/chatbot/config.js
@@ -1,24 +1,27 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAFeWyKYN50BAGde7NOFEsLCgIZCzN5k3JD24XO4CqOTCaY506P7oi2a8Ai1qWhhS9zOs53VFWVTMbRJ6E5b5VY4SV5cNZBpatSvCtsZBuxp6AW9sHkUw2dzDOWv9eZC4tvRiUreQlyNKebALzKDoAZCLrRkHwIDuWwWTxZCsAUwZAUgoSCS8Mx',
-    FB_VERIFY_TOKEN: 'CajaTacna.2017',
-    FB_APP_SECRET: '7ce7ec62e32b0ed04c03b48df5a38686',
-    SERVER_URL: 'https://botcertificacion.herokuapp.com/',
-    GOOGLE_PROJECT_ID: 'newagent-xouosp',
-    DF_LANGUAGE_CODE: 'es',
-    GOOGLE_CLIENT_EMAIL: 'charlybot@newagent-xouosp.iam.gserviceaccount.com',
-    GOOGLE_PRIVATE_KEY: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDXq2VczJTS1gs/\ncBkKLsm9fMMbyODtYUjjaVEF0XXhMfFRxc30S93V5RAU7mjGutYVLYfE5D7s4rrX\nwpJI5+YzOHkkeJNS3t62/SSWcpcyODWsopEJP0WYnDq8Aj9OaqacSLLlOxozLg4u\nnEsYQsN7xVlplEgu8xuDwxsEls+jO5yN/dS1JzSiQET/9tfpYeLnydOznEgTcO7y\nQ1AN6LP6MZ/tjgBKhDVTXf11AotGpk7cwwAzFAuOx7WaJncgLzAeW9qe2vRm5UpR\nnL9SNej1ATZnTCqIa1X9/k26brXr3gC4Mk3kB+cWV2LAPbLjKutwP6nYlIWyZ9HQ\n11SSPIGZAgMBAAECggEAG2XJJyxQHaaWYDU34RzHgRxn1tBCjrVkoosfAVu2G2kS\nTH6R5t56bHFAhUAyLEjQ6HjTvhX2znYRIVa06bEYA9rBPPpaUNVCOsHW04v0HCua\n2fb+8MtKZpQ2O+sXKrmOt9zeaROEhQrtP0uYmJjmp4MPFEQDlX9JuAfhl0gnUYeF\n7KZlm9T7auTlA4qQjICi6pfj0/orK7912r1f37GnA7/aHAeznsk09csCf0e8gtEC\nDVcHSk6tY2Kv81O8PPglCSAysmZ9BMZHeFNON/vRYNJJy0AIxqdNbtDepcyYUB+n\nTqX8vxhw428ZTf7TGB2JvQOCWCQ2aj5QNoFjb9R12QKBgQDs4ftvVbRmdOqk8wm6\n6TGKRooC+/9U2k2BeVhlEFgYWYVBnmij6wWcU7yuobsSl/GDyyaqSP0Khi1FXQ3G\nddjFdsWRK20DjUpvuYIqcW+uT0WRfydEMh3pnGx3pjqv/KXr9rRQ/JPoLoDrbBaQ\n2Z9N3laurYzjIUltJmkpamWnVQKBgQDpEyWMpcS2eTaSVqTxXbDJcMeS+j7+BMTb\nMZK6AWB+rjjpHQQCT1NvGmnQBfYpkFtBqBflZNM6OYHQSEx4IJIZn3o/1r4i7pEF\np5+hdfeGGdXyQSbxmc8qa8y8pPYrFniYKtgdvcxGvSierzY2aQgy/h+MpuJ5JkJe\n69h0NVNpNQKBgQCQGjkW1ZBU87L6fU7iAZI9iAPVJO+C8vpLVUJM2NB+n5bNAEyv\nyv0yyE3z/jcLbJtjjWYMGTK2AEHx/FWw8WfZWBN/6Gb8zisGJFfm8fL6mwa7KBr/\nBW3k+1+QufLhtFRLY1z2F3Dywbdzf+GKZJjgie05fiJC2bENUVyP8UW9sQKBgDo8\nXXp6BN+vA7Km0Ym5Zi2+7y7LNigUGvfFuCqfo3OjpnagmFrXZTpyeBPRGYOjHRoX\nnPjlK1319niLSAgP41FhppMC3fkucK/gBSO7rQAlvm5MVyWwz7GCw+IyuYtkjvc+\nZUdxH/Rg2SBlGP+ZWMIc+tcS761joNvExA2tMU0tAoGADxedRMdD4orwhndRIOjv\nmBh3BgzVsGIIYvpGz0KVAvc94dbkSvCAVPYgcbP9KkZucaHFQRz5lnm6DN09/kaF\nnRP80kLDCYtXoanIHcHlNOZ8LfZINP1ddYIr9ig9Up5TsM+iuKlSWxEGshtgnhgm\nBVEFEycs80/yyeXx3YBpQWM=\n-----END PRIVATE KEY-----\n',
-    KEYFILENAME: __dirname + '/newagent-xouosp-b8ae8d516e4d.json', //credenciales
-    //database
-    PG_CONFIG: {
-        user: 'wwvuhnknbmkper',
-        database: 'dbvkd6arsvm0oj',
-        password: '860dcbaa12bf159c077c1f285b57fb8155f7ea9ad2c0c47f9e3d822f8390e2d3',
-        host: 'ec2-54-83-33-14.compute-1.amazonaws.com',
-        port: 5432,
-        max: 10,
-        idleTimeoutMillis: 30000,
-    },
-    // jwt
-    SEED: 'mySecredSeed',
-    CADUCIDAD: 1 * 60 //one minute
+  FB_PAGE_TOKEN:
+    "EAAFeWyKYN50BAKQwT7yOCkyaoozizi2ElFM5bCineKGUj8WuzByGE0Pry22OqJegpHgtsHb5Sb7D7NdvFZB15m0MiZC2tVRVyI8Lvv6tSRZCeSH9Nhh6LWfutwpkXaUevkW6x7xQvnOx9Q2PXHq3Hvsv9BSALqsZCzUSrgwBCh6FgJ3ggWNp",
+  FB_VERIFY_TOKEN: "CajaTacna.2017",
+  FB_APP_SECRET: "7ce7ec62e32b0ed04c03b48df5a38686",
+  SERVER_URL: "https://botcertificacion.herokuapp.com/",
+  GOOGLE_PROJECT_ID: "newagent-xouosp",
+  DF_LANGUAGE_CODE: "es",
+  GOOGLE_CLIENT_EMAIL: "charlybot@newagent-xouosp.iam.gserviceaccount.com",
+  GOOGLE_PRIVATE_KEY:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDXq2VczJTS1gs/\ncBkKLsm9fMMbyODtYUjjaVEF0XXhMfFRxc30S93V5RAU7mjGutYVLYfE5D7s4rrX\nwpJI5+YzOHkkeJNS3t62/SSWcpcyODWsopEJP0WYnDq8Aj9OaqacSLLlOxozLg4u\nnEsYQsN7xVlplEgu8xuDwxsEls+jO5yN/dS1JzSiQET/9tfpYeLnydOznEgTcO7y\nQ1AN6LP6MZ/tjgBKhDVTXf11AotGpk7cwwAzFAuOx7WaJncgLzAeW9qe2vRm5UpR\nnL9SNej1ATZnTCqIa1X9/k26brXr3gC4Mk3kB+cWV2LAPbLjKutwP6nYlIWyZ9HQ\n11SSPIGZAgMBAAECggEAG2XJJyxQHaaWYDU34RzHgRxn1tBCjrVkoosfAVu2G2kS\nTH6R5t56bHFAhUAyLEjQ6HjTvhX2znYRIVa06bEYA9rBPPpaUNVCOsHW04v0HCua\n2fb+8MtKZpQ2O+sXKrmOt9zeaROEhQrtP0uYmJjmp4MPFEQDlX9JuAfhl0gnUYeF\n7KZlm9T7auTlA4qQjICi6pfj0/orK7912r1f37GnA7/aHAeznsk09csCf0e8gtEC\nDVcHSk6tY2Kv81O8PPglCSAysmZ9BMZHeFNON/vRYNJJy0AIxqdNbtDepcyYUB+n\nTqX8vxhw428ZTf7TGB2JvQOCWCQ2aj5QNoFjb9R12QKBgQDs4ftvVbRmdOqk8wm6\n6TGKRooC+/9U2k2BeVhlEFgYWYVBnmij6wWcU7yuobsSl/GDyyaqSP0Khi1FXQ3G\nddjFdsWRK20DjUpvuYIqcW+uT0WRfydEMh3pnGx3pjqv/KXr9rRQ/JPoLoDrbBaQ\n2Z9N3laurYzjIUltJmkpamWnVQKBgQDpEyWMpcS2eTaSVqTxXbDJcMeS+j7+BMTb\nMZK6AWB+rjjpHQQCT1NvGmnQBfYpkFtBqBflZNM6OYHQSEx4IJIZn3o/1r4i7pEF\np5+hdfeGGdXyQSbxmc8qa8y8pPYrFniYKtgdvcxGvSierzY2aQgy/h+MpuJ5JkJe\n69h0NVNpNQKBgQCQGjkW1ZBU87L6fU7iAZI9iAPVJO+C8vpLVUJM2NB+n5bNAEyv\nyv0yyE3z/jcLbJtjjWYMGTK2AEHx/FWw8WfZWBN/6Gb8zisGJFfm8fL6mwa7KBr/\nBW3k+1+QufLhtFRLY1z2F3Dywbdzf+GKZJjgie05fiJC2bENUVyP8UW9sQKBgDo8\nXXp6BN+vA7Km0Ym5Zi2+7y7LNigUGvfFuCqfo3OjpnagmFrXZTpyeBPRGYOjHRoX\nnPjlK1319niLSAgP41FhppMC3fkucK/gBSO7rQAlvm5MVyWwz7GCw+IyuYtkjvc+\nZUdxH/Rg2SBlGP+ZWMIc+tcS761joNvExA2tMU0tAoGADxedRMdD4orwhndRIOjv\nmBh3BgzVsGIIYvpGz0KVAvc94dbkSvCAVPYgcbP9KkZucaHFQRz5lnm6DN09/kaF\nnRP80kLDCYtXoanIHcHlNOZ8LfZINP1ddYIr9ig9Up5TsM+iuKlSWxEGshtgnhgm\nBVEFEycs80/yyeXx3YBpQWM=\n-----END PRIVATE KEY-----\n",
+  KEYFILENAME: __dirname + "/newagent-xouosp-b8ae8d516e4d.json", //credenciales
+  //database
+  PG_CONFIG: {
+    user: "wwvuhnknbmkper",
+    database: "dbvkd6arsvm0oj",
+    password:
+      "860dcbaa12bf159c077c1f285b57fb8155f7ea9ad2c0c47f9e3d822f8390e2d3",
+    host: "ec2-54-83-33-14.compute-1.amazonaws.com",
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+  },
+  // jwt
+  SEED: "mySecredSeed",
+  CADUCIDAD: 1 * 60 //one minute
 };
--- server/index.js
+++ server/index.js
@@ -1,46 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 //auth
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}))
-
+  })
+);
+//2020
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // app.use(cors());
 // CORS middleware
-const allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    next();
-}
+const allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+};
 
-app.use(allowCrossDomain)
+app.use(allowCrossDomain);
 
-const routes = require('./routes/api/api.js');
-const webhook = require('./chatbot/app.js');
-app.use('/api', routes);
-app.use('/api', webhook);
+const routes = require("./routes/api/api.js");
+const webhook = require("./chatbot/app.js");
+app.use("/api", routes);
+app.use("/api", webhook);
 
-//Handle Production
-if (process.env.NODE_ENV === 'production') {
-    //static folder
-    app.use(express.static(__dirname + '/public'));
-    //Handle SPA
-    app.get(/.*/, (req, res) => {
-        res.sendFile(__dirname + '/public/index.html')
-    });
+if (process.env.NODE_ENV === "production") {
+  //static folder
+  app.use(express.static(__dirname + "/public"));
+  //Handle SPA
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
 }
 process.env.PORT = process.env.PORT || 8000;
 app.listen(process.env.PORT, () => {
-    console.log(`Server starting on port ${process.env.PORT}`);
+  console.log(`Server starting on port ${process.env.PORT}`);
 });
